5153f38a4845decf241ac938ef1b01984471da7e
wtotw <s135061@gmail.com>
2020-03-09 00:13:24 +0900

    webpacker実行（動かない）

---
+++ rails/app/helpers/hello_vue_helper.rb
@@ -0,0 +1,2 @@
+module HelloVueHelper
+end
--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'hello_vue/index'
   get 'top/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
